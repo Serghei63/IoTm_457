17949a56da3521526088bc3dac2a5492a5c4a837
Mit4el <mit4bmw@gmail.com>
2024-11-27 01:00:43 +0300

    undo ~, try exception

--- tools/patch32_ws.py
+++ tools/patch32_ws.py
@@ -10,18 +10,20 @@ from sys import platform
 
 if platform == "linux" or platform == "linux2":
     # linux
-    mainPyPath = '~/.platformio/packages/framework-arduinoespressif32/libraries/WiFi/src/WiFiClient.cpp'
+    mainPyPath = '/home/rise/.platformio/packages/framework-arduinoespressif32/libraries/WiFi/src/WiFiClient.cpp'
 else:
     # windows
     mainPyPath = os.environ['USERPROFILE'] + '\\.platformio\\packages\\framework-arduinoespressif32\\libraries\\WiFi\\src\\WiFiClient.cpp'
 
 # print(mainPyPath)
-
-with open(mainPyPath) as fr:
-    oldData = fr.read()
-    if not 'if WIFI_CLIENT_MAX_WRITE_RETRY      (10)' in oldData:
-        shutil.copyfile(mainPyPath, mainPyPath+'.bak')
-        newData = oldData.replace('#define WIFI_CLIENT_MAX_WRITE_RETRY      (10)', '#define WIFI_CLIENT_MAX_WRITE_RETRY      (2)')
-        newData = newData.replace('#define WIFI_CLIENT_SELECT_TIMEOUT_US    (1000000)', '#define WIFI_CLIENT_SELECT_TIMEOUT_US    (500000)')
-        with open(mainPyPath, 'w') as fw:
-            fw.write(newData)
+try:
+    with open(mainPyPath) as fr:
+        oldData = fr.read()
+        if not 'if WIFI_CLIENT_MAX_WRITE_RETRY      (10)' in oldData:
+            shutil.copyfile(mainPyPath, mainPyPath+'.bak')
+            newData = oldData.replace('#define WIFI_CLIENT_MAX_WRITE_RETRY      (10)', '#define WIFI_CLIENT_MAX_WRITE_RETRY      (2)')
+            newData = newData.replace('#define WIFI_CLIENT_SELECT_TIMEOUT_US    (1000000)', '#define WIFI_CLIENT_SELECT_TIMEOUT_US    (500000)')
+            with open(mainPyPath, 'w') as fw:
+                fw.write(newData)
+except FileNotFoundError:
+    print("Файл не найден или не удается открыть")
--- tools/patch32c6.py
+++ tools/patch32c6.py
@@ -5,25 +5,28 @@ from sys import platform
 
 if platform == "linux" or platform == "linux2":
     # linux
-    devkitm = '~/.platformio/platforms/espressif32/boards/esp32-c6-devkitm-1.json'
-    devkitc = '~/.platformio/platforms/espressif32/boards/esp32-c6-devkitc-1.json'    
+    devkitm = '/home/rise/.platformio/platforms/espressif32/boards/esp32-c6-devkitm-1.json'
+    devkitc = '/home/rise/.platformio/platforms/espressif32/boards/esp32-c6-devkitc-1.json'    
 else:
     # windows
     devkitm = os.environ['USERPROFILE'] + '\\.platformio\\platforms\\espressif32\\boards\\esp32-c6-devkitm-1.json'
     devkitc = os.environ['USERPROFILE'] + '\\.platformio\\platforms\\espressif32\\boards\\esp32-c6-devkitc-1.json'    
 
 def add_arduino_to_frameworks(file_name):
-    with open(file_name, 'r+') as f:
-        data = json.load(f)
-        frameworks = data['frameworks']
-        if 'arduino' not in frameworks:
-            frameworks.insert(frameworks.index('espidf') + 1, 'arduino')
-            data['frameworks'] = frameworks
-            f.seek(0)
-            json.dump(data, f, indent=4)
-            f.truncate()
-        else:
-            print(f"Arduino already exists in {file_name}")
+    try:
+        with open(file_name, 'r+') as f:
+            data = json.load(f)
+            frameworks = data['frameworks']
+            if 'arduino' not in frameworks:
+                frameworks.insert(frameworks.index('espidf') + 1, 'arduino')
+                data['frameworks'] = frameworks
+                f.seek(0)
+                json.dump(data, f, indent=4)
+                f.truncate()
+            else:
+                print(f"Arduino already exists in {file_name}")
+    except FileNotFoundError:
+        print("Файл не найден или не удается открыть")
 
 if os.path.exists(devkitm) and os.path.exists(devkitc):
     add_arduino_to_frameworks(devkitm)
