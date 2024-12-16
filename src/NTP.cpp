ca7b3890cb5530a8b44386988049a68613ed507e
Serghei63 <krasnikov.s.v@gmail.com>
2024-12-16 15:38:50 +0200

    День недели в сценариях

--- src/NTP.cpp
+++ src/NTP.cpp
@@ -185,6 +185,39 @@ unsigned long strDateToUnix(String date) {
     return (day * secsInOneDay) + (numberOfDaysInPastMonths * secsInOneDay) + (totalNormalYears * daysInOneYear * secsInOneDay) + (numberOfLeepYears * daysInLeepYear * secsInOneDay);
 }
 
+//---------------------------
+/*
+ * Локальное дата время "день недели"
+ */
+  
+   const String getTimeLocal_Weekday() {
+       char buf[32];
+       sprintf(buf, "%d", _time_local.day_of_week);
+       switch (_time_local.day_of_week) {
+  
+  
+      
+      
+   case 1: return "Воскресенье"; break;
+   case 2: return "Понедельник"; break;
+   case 3: return "Вторник"; break;
+   case 4: return "Среда"; break;
+   case 5: return "Четверг"; break;
+   case 6: return "Пятница"; break;
+   case 7: return "Суббота"; break;
+  // case 1: return "7"; break;
+  // case 2: return "1"; break;
+ //  case 3: return "2"; break;
+ //  case 4: return "3"; break;
+  // case 5: return "4"; break;
+  // case 6: return "5"; break;
+ //  case 7: return "6"; break;
+    default: return ""; break;         
+    }
+    return String(buf);
+   }
+   //-----------------------------------------------------------------
+
 const String getDateTimeDotFormatedFromUnix(unsigned long unixTime) {
     Time_t time;
     breakEpochToTime(unixTime, time);
