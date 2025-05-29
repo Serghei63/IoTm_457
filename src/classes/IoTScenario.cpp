1a79776bfbd8e7e9489b986c703745aa73023c3c
Ilya <batata@mail.ru>
2025-05-29 18:57:46 +0300

    Добавляем системные вызовы setInterval и doByInterval Для возможности изменения интервала выполнения doByInterval и вызова его вне плана

--- src/classes/IoTScenario.cpp
+++ src/classes/IoTScenario.cpp
@@ -291,10 +291,8 @@ class CallExprAST : public ExprAST {
             ret.valD = Item->getIntFromNet();
             ret.isDecimal = true;
             return &ret;
-        }
-
-        // вызываем системную функцию периодического выполнения вне таймера
-        if (Cmd == F("doByInterval")) {
+        
+        } else if (Cmd == F("doByInterval")) {  // вызываем системную функцию периодического выполнения вне таймера
             Item->doByInterval();
             ret = Item->value;
             return &ret;
@@ -309,6 +307,15 @@ class CallExprAST : public ExprAST {
                 ArgsAsIoTValue.push_back(*tmp);
             else
                 return nullptr;  // ArgsAsIoTValue.push_back(zeroIotVal);
+        }
+
+        if (Cmd == F("setInterval")) {  // меняем интервал выполнения задач модуля налету 
+            if (ArgsAsIoTValue.size() == 1) {
+                Item->setInterval(ArgsAsIoTValue[0].valD);
+                ret.valD = Item->getInterval();
+                ret.isDecimal = true;
+                return &ret;
+            }
         }
 
         ret = Item->execute(Cmd, ArgsAsIoTValue);  // вызываем команду из модуля напрямую с передачей всех аргументов
@@ -350,7 +357,8 @@ enum SysOp {
     sysop_mqttPub,
     sysop_getUptime,
     sysop_mqttIsConnect,
-    sysop_wifiIsConnect
+    sysop_wifiIsConnect,
+    sysop_setInterval
 };
 
 IoTValue sysExecute(SysOp command, std::vector<IoTValue> &param) {
@@ -456,7 +464,12 @@ IoTValue sysExecute(SysOp command, std::vector<IoTValue> &param) {
             break;
         case sysop_wifiIsConnect:
             value.valD = isNetworkActive();
-            break;            
+            break;
+        case sysop_setInterval:
+            if (param.size() == 1) {
+                
+            }
+            break;
     }
 
     return value;
@@ -515,6 +528,8 @@ class SysCallExprAST : public ExprAST {
             operation = sysop_mqttIsConnect;
         else if (Callee == F("wifiIsConnect"))
             operation = sysop_wifiIsConnect;            
+        else if (Callee == F("setInterval"))
+            operation = sysop_setInterval;
         else
             operation = sysop_notfound;
     }
