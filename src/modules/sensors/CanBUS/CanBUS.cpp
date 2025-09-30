b7f79f1e60eea6c0529cd95da2092e0038c4dcf5
Serghei63 <krasnikov.s.v@gmail.com>
2025-09-30 16:06:10 +0300

    Отправка по шине

--- src/modules/sensors/CanBUS/CanBUS.cpp
+++ src/modules/sensors/CanBUS/CanBUS.cpp
@@ -89,6 +89,95 @@ public:
 
 };
 
+class CanBUSs : public IoTItem
+
+{
+private:
+ String _regStr1 = "";  // Адрес регистра который будем дергать ( по коду от 0х0000 до 0х????)
+ String _regAsk = "";  // Адрес регистра который будем дергать ( по коду от 0х0000 до 0х????)
+// String _dlcStr = "";  // длинна данных (Data Length Code)
+ uint16_t _reg1 = 0;
+// uint16_t _ask = 0;
+ uint8_t _adr = 0;    // Адрес слейва от 1 до 247
+ uint8_t _dlc = 0;     // Длина данных (Data Length Code)
+
+public:
+
+  CanBUSs(String parameters) : IoTItem(parameters)
+  {
+
+   
+        // адреса регистров прочитаем с веба
+      _adr = jsonReadInt(parameters, "adr"); 
+      _dlc = jsonReadInt(parameters, "dlc"); // Длина данных (Data Length Code) по умолчанию 8
+      jsonRead(parameters, "cans_id", _regStr1);    // адреса регистров прочитаем с веба
+      _reg1 = hexStringToUint16(_regStr1);
+    //  jsonRead(parameters, "ask_id", _regAsk);    // адреса регистров прочитаем с веба
+    //  _ask = hexStringToUint16(_regAsk);
+      jsonRead(parameters, "debug", _debug);
+      SPI.begin();                                                 //Begins SPI communication
+
+           mcp2515.reset();
+           mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);              //Sets CAN at speed 500KBPS and Clock 8MHz
+           mcp2515.setNormalMode();                                //Sets CAN at normal mode
+
+  }
+
+        // Комманды из сценария
+  IoTValue execute(String command, std::vector<IoTValue> &param)
+  {
+    IoTValue val;
+
+    uint16_t _reg1 = 0;
+
+   if (mcp2515.sendMessage(&canMsg) == MCP2515::ERROR_OK)  
+    
+   {
+
+    //  if (param.size())
+         if (command == "sendMsg") // vout = mb.writeSingleRegister(1,"0x0003", 1) - addr, register, state
+    {
+          if (param.size()) 
+        {
+      //jsonRead(parameters, "cans_id", _regStr1);    // адреса регистров прочитаем с веба
+    //  _reg1 = hexStringToUint16(_regStr1);
+        
+        _reg1 = hexStringToUint16(param[0].valS);
+        _dlc = param[1].valD;
+       // _adr = param[2].valD;
+        uint16_t data = param[2].valD;
+
+           // canMsg.can_id = 0x036;
+           canMsg.can_id = _reg1;
+            canMsg.can_dlc = 1;
+            canMsg.data[0] = data;
+  
+
+               mcp2515.sendMessage(&canMsg);     //Sends the CAN message
+
+     if (_debug)
+        {
+       //   SerialPrint("I", "CanBUS", "sendMsg, regStr: " + _regStr1 + ", dlc: " + String(_dlc, DEC) + ", adres: " + String(_adr, DEC) + ", data: " + String(data));
+         SerialPrint("I", "CanBUS", "sendMsg, registr: " + String(_reg1, HEX) + ", adres: " + String(_adr, DEC) + ", data: " + String(data));
+        }
+
+   // mcp2515.sendMessage(&canMsg);     //Sends the CAN message
+          }
+      
+      return {};
+    }
+return val;
+  }
+
+
+  };
+
+    ~CanBUSs(){
+    
+  };
+
+};
+
 void *getAPI_CanBUS(String subtype, String param)
 {
 
@@ -96,6 +185,11 @@ void *getAPI_CanBUS(String subtype, String param)
   {
     return new CanBUSm(param);
   }
+  else if (subtype == F("canSlave"))
+  {
+    return new CanBUSs(param);
+  }
+  else
 
   {
     return nullptr;
